1b60f18bf6afa91b9f695cba867c86d75b7479ed
Brijen Makwana <brijenmakwana@Brijens-MacBook-Air.local>
2022-03-14 16:50:17 +0530

    Fetch new Advice every time, icons added

--- Content.js
+++ Content.js
@@ -1,18 +1,26 @@
+// HTML content
+
 const generateHTML = (pageName) => {
   return `<div>
 
   <h1>Your <b>Work</b> is more important than <b>${pageName}</b></h1>
   <h3>So stop wasting your time on this and focus on your work.</h3>
+  <i id="advice">Loading...</i><br/>
   <p>
     Made with ❤️ by Brijen Makwana
   </p>
 </div>`;
 };
 
+//css styling for the HTML
+
 const generateSTYLES = () => {
   return ` 
     <style>
-  
+        html.default{
+            background-color: #fff;
+        }
+        
         body {
             font-family: 'Helvetica Neue', Arial, sans-serif;
             background-image: url('https://images.unsplash.com/photo-1472289065668-ce650ac443d2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2069&q=80');
@@ -58,13 +66,34 @@ const generateSTYLES = () => {
             position: absolute; 
             bottom: 5px;
             right: 20px;
-            font-size: 1.5vw;
+            font-size: 1.2vw;
             font-style: italic;
             color: #000
+        }
+
+        i{
+            position: absolute; 
+            bottom: 100px;
+            right: 20px;
+            color: #02383C;
+            font-size: 1.4vw;
+            width: 20%;
         }
   
     </style>`;
 };
+
+// fetch new advice every time website loads
+
+fetch("https://api.adviceslip.com/advice")
+  .then((res) => res.json())
+  .then((res) => {
+    console.log(res.slip.advice);
+    const h = document.getElementById("advice");
+    h.innerText = res.slip.advice;
+  });
+
+// prevent this sites from opening
 
 switch (window.location.hostname) {
   case "m.facebook.com":
