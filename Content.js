995a96d0a2e4b45830d6791b2545cb6ee6e0c62e
Brijen Makwana <brijenmakwana@Brijens-MacBook-Air.local>
2022-03-28 10:36:51 +0530

    Code refactor

--- Content.js
+++ Content.js
@@ -5,7 +5,6 @@ const generateHTML = (pageName) => {
 
   <h1>Your <b>Work</b> is more important than <b>${pageName}</b></h1>
   <h3>So stop wasting your time on this and focus on your work.</h3>
-  <p id="advice">Loading...</p><br/>
   <i>
     Made with ❤️ by Brijen Makwana
   </i>
@@ -17,16 +16,14 @@ const generateHTML = (pageName) => {
 const generateSTYLES = () => {
   return ` 
     <style>
-        html.default{
-            background-color: #CFE1B9;        }
-        
         body {
             font-family: 'Helvetica Neue', Arial, sans-serif;
-            background-color: #CFE1B9;
+            background-image: linear-gradient(to right, #ff1b6b , #45caff) !important;
+            height: 100% !important;
         }
 
         h1{
-            color: #1B4332;
+            color: #000;
             font-size: 3vw;
             font-style: italic;
             text-align: center;
@@ -44,13 +41,13 @@ const generateSTYLES = () => {
         }
 
         @keyframes text {
-          0%   {color: #e63946;}
-          50%  {color: #000;}
-          100% {color: #e63946;}
+          0%   {color: #000;}
+          50%  {color: #fff;}
+          100% {color: #000;}
         }
 
         h3{
-            color: #1B4332;
+            color: #000;
             font-size: 1.5vw;
             font-style: italic; 
             text-align:  center;
@@ -58,10 +55,10 @@ const generateSTYLES = () => {
 
 
         p{
-          position: absolute; 
+            position: absolute; 
             bottom: 100px;
             right: 20px;
-            color: #02383C;
+            color: #fff;
             font-size: 1.4vw;
             width: 20%;  
         }
@@ -79,56 +76,69 @@ const generateSTYLES = () => {
     `;
 };
 
+// fetch new advice every time website loads
+
+function getAdvice() {
+  fetch("https://api.adviceslip.com/advice")
+    .then((res) => res.json())
+    .then((res) => {
+      const p = document.createElement("p");
+      p.innerText = res.slip.advice;
+      document.body.appendChild(p);
+    });
+}
+
 // prevent this sites from opening
 
 switch (window.location.hostname) {
   case "m.facebook.com":
     document.head.innerHTML = generateSTYLES();
     document.body.innerHTML = generateHTML("FACEBOOK");
+    getAdvice();
     break;
   case "www.facebook.com":
     document.head.innerHTML = generateSTYLES();
     document.body.innerHTML = generateHTML("FACEBOOK");
+    getAdvice();
     break;
   case "www.netflix.com":
     document.head.innerHTML = generateSTYLES();
     document.body.innerHTML = generateHTML("NETFLIX");
+    getAdvice();
     break;
   case "www.hotstar.com":
     document.head.innerHTML = generateSTYLES();
     document.body.innerHTML = generateHTML("HOTSTAR");
+    getAdvice();
     break;
   case "www.instagram.com":
     document.head.innerHTML = generateSTYLES();
     document.body.innerHTML = generateHTML("INSTAGRAM");
+    getAdvice();
     break;
   case "www.snapchat.com":
     document.head.innerHTML = generateSTYLES();
     document.body.innerHTML = generateHTML("SNAPCHAT");
+    getAdvice();
     break;
   case "www.sonyliv.com":
     document.head.innerHTML = generateSTYLES();
     document.body.innerHTML = generateHTML("SONYLIVE");
+    getAdvice();
     break;
   case "www.zee5.com":
     document.head.innerHTML = generateSTYLES();
     document.body.innerHTML = generateHTML("ZEE5");
+    getAdvice();
     break;
   case "www.altbalaji.com":
     document.head.innerHTML = generateSTYLES();
     document.body.innerHTML = generateHTML("ALTBALAJI");
+    getAdvice();
     break;
   case "www.mxplayer.in":
     document.head.innerHTML = generateSTYLES();
     document.body.innerHTML = generateHTML("MXPLAYER");
+    getAdvice();
     break;
 }
-
-// fetch new advice every time website loads
-
-fetch("https://api.adviceslip.com/advice")
-  .then((res) => res.json())
-  .then((res) => {
-    const p = document.getElementById("advice");
-    p.innerText = res.slip.advice;
-  });
